132e2fed12050f0fcc07dbbba9557ec2750480c0
Andi <andi.hanus@gmx.de>
2013-09-17 14:48:50 +0200

    updated json.php
    new features:
    * embeded podio api field request to extend fields with meta data (caching support by layer 1)
    * updated structure in items.json files

--- json.php
+++ json.php
@@ -13,11 +13,28 @@ require_once PATH_LIB_PODIO.'/PodioAPI.php';
 Podio::setup(PODIO_USER_ID, PODIO_USER_SECRET);
 Podio::authenticate('app', array('app_id' => PODIO_APP_ID, 'app_token' => PODIO_APP_TOKEN));
 
+// get podio's app field definitions
+if ($podioConfig['filter_cache'] >= 1 && file_exists(PATH_TMP.'/podioAppInfo.cache')) $podioAppInfoResponse = unserialize(file_get_contents(PATH_TMP.'/podioAppInfo.cache'));
+else {
+  $podioAppInfoRequest = Podio::request('GET', '/app/'.PODIO_APP_ID);
+  if (!$podioAppInfoRequest || $podioAppInfoRequest->status != 200) {
+    die('workflow aborted api get:/app/'.PODIO_APP_ID);
+  }
+  $podioAppInfoResponse = json_decode($podioAppInfoRequest->body);
+
+  if ($podioConfig['filter_cache'] >= 1) file_put_contents(PATH_TMP.'/podioAppInfo.cache', serialize($podioAppInfoResponse));
+}
+$podioFieldData = array();
+foreach ($podioAppInfoResponse->fields as $fieldData) {
+  $podioFieldData[$fieldData->external_id] = $fieldData;
+}
+
+// count rows in podio app
 if ($podioConfig['filter_cache'] === 2 && file_exists(PATH_TMP.'/podioAppItemCount.cache')) $podioAppCountResponse = unserialize(file_get_contents(PATH_TMP.'/podioAppItemCount.cache'));
 else {
   $podioAppCountRequest = Podio::request('GET', '/item/app/'.PODIO_APP_ID.'/count');
   if (!$podioAppCountRequest || $podioAppCountRequest->status != 200) {
-    die('workflow aborted');
+    die('workflow aborted api get:/item/app/'.PODIO_APP_ID.'/count');
   }
   $podioAppCountResponse = json_decode($podioAppCountRequest->body);
   
@@ -98,31 +115,18 @@ for ($i=0;$i<$podioFilterRequestRuns;$i++) {
         }
         //check if field is visible
         if (in_array($itemRawField->external_id, $viewConfig['fields'])) {
-          $podioViewData[$viewKey][$itemRaw->item_id]['fields'][$itemRawField->external_id] = array(
-            #'type' => $itemRawField->type,
-            #'raw' => '',
-            #'value' => '',
-          );
+          $podioViewData[$viewKey][$itemRaw->item_id]['fields'][$itemRawField->external_id] = array('values' => array());
           foreach ($itemRawField->values as $num => $itemRawFieldValue) {
             switch ($itemRawField->type) {
             	case 'text':
             	case 'state':
-            	  $podioViewData[$viewKey][$itemRaw->item_id]['fields'][$itemRawField->external_id][$num] = array(
+            	  $podioViewData[$viewKey][$itemRaw->item_id]['fields'][$itemRawField->external_id]['values'][$num] = array(
             	  	'raw' => $itemRawFieldValue->value,
             	  	'value' => $itemRawFieldValue->value,
             	  );
             	  break;
             }
           }
-          /*
-          switch ($itemRawField->type) {
-            case 'text':
-            case 'state':
-                $podioViewData[$viewKey][$itemRaw->item_id]['fields'][$itemRawField->external_id]['raw'] = htmlspecialchars($itemRawField->values[0]->value);
-                $podioViewData[$viewKey][$itemRaw->item_id]['fields'][$itemRawField->external_id]['value'] = htmlspecialchars($itemRawField->values[0]->value);
-              break;
-          }
-          */
         }
       }
     }
@@ -146,8 +150,13 @@ foreach ($jsonView as $viewKey => $viewConfig) {
     // podio api gives not meta data of empty fields, so we add 'em
     foreach ($viewConfig['fields'] as $fieldExternalId) {
       if (!isset($podioViewData[$viewKey][$itemId]['fields'][$fieldExternalId])) {
-        $podioViewData[$viewKey][$itemId]['fields'][$fieldExternalId] = array();
+        $podioViewData[$viewKey][$itemId]['fields'][$fieldExternalId] = array('values' => array());
       }
+      // embed field meta data
+      $podioViewData[$viewKey][$itemId]['fields'][$fieldExternalId]['external_id'] = $fieldExternalId;
+      $podioViewData[$viewKey][$itemId]['fields'][$fieldExternalId]['type'] = $podioFieldData[$fieldExternalId]->type;
+      $podioViewData[$viewKey][$itemId]['fields'][$fieldExternalId]['label'] = $podioFieldData[$fieldExternalId]->config->label;
+      $podioViewData[$viewKey][$itemId]['fields'][$fieldExternalId]['weight'] = $podioFieldData[$fieldExternalId]->config->delta;
     }
     
     // link field result to $jsonViewData
@@ -158,8 +167,3 @@ foreach ($jsonView as $viewKey => $viewConfig) {
   if (!is_dir(PATH_HTDOCS.'/'.$viewKey)) mkdir(PATH_HTDOCS.'/'.$viewKey);
   file_put_contents(PATH_HTDOCS.'/'.$viewKey.'/'.PODIO_JSON_FILE_NAME, json_encode($jsonViewData[$viewKey]));
 }
-
-#print "COUNT VIEW: ".count($podioViewData[1])."\n";
-#print "COUNT VIEW: ".count($podioViewData[2])."\n";
-#print_r($jsonViewData);
-#print_r(array_shift($podioItemRawData));
